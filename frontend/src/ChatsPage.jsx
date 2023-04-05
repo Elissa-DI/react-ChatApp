eea3192fd385171fa688d99d3e1df28f34c5fcfa
Elissa <elissadusabe@gmail.com>
2023-04-05 02:58:33 +0200

    Done with the last added codes

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,14 +1,13 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        '6c64811b-5938-4d9b-ab6d-2f3750b9136a',
-         props.user.username, 
-         props.user.secret
-    );
     return (
          <div style={{ height: '100vh' }}>
-            <MultiChatSocket { ...chatProps } />
-            <MultiChatWindow { ...chatProps } style={{ height: '100%' }} />
+            <PrettyChatWindow
+                projectId='6c64811b-5938-4d9b-ab6d-2f3750b9136a'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100vh' }}
+            />
          </div>
     );
 };
